70b1b221a6ebd980ef1226b27b3613c8b66e34a0
Oscar Gu <oscar.gu@philips.com>
2022-12-08 19:50:46 +0800

    init

---
+++ Dao.QueueExecutor/Catcher.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Dao.QueueExecutor
+{
+    public class Catcher
+    {
+        readonly SemaphoreSlim locker;
+        volatile bool incoming;
+
+        public Catcher(int concurrentCount = 1)
+        {
+            if (concurrentCount < 1)
+                concurrentCount = 1;
+
+            this.locker = new SemaphoreSlim(concurrentCount, concurrentCount);
+        }
+
+        /// <summary>
+        /// Set the catcher to catch the ball.
+        /// </summary>
+        public event Func<Task> Catch;
+        public event Action<Exception> OnException;
+
+        /// <summary>
+        /// Throw a ball in any time, and there would be at least one catcher to catch the ball.
+        /// </summary>
+        public void Throw()
+        {
+            this.incoming = true;
+
+            if (this.locker.CurrentCount < 1)
+                return;
+
+            Task.Run(Handle);
+        }
+
+        async Task<Func<Task>> GetHandler()
+        {
+            while (true)
+            {
+                var result = Catch;
+                if (result != null)
+                    return result;
+
+                await Task.Delay(15).ConfigureAwait(false);
+            }
+        }
+
+        async Task Handle()
+        {
+            do
+            {
+                this.incoming = false;
+
+                await this.locker.WaitAsync().ConfigureAwait(false);
+                try
+                {
+                    var @catch = await GetHandler();
+                    await @catch();
+                }
+                catch (Exception ex)
+                {
+                    var onException = OnException;
+                    if (onException != null)
+                    {
+                        try
+                        {
+                            onException(ex);
+                        }
+                        catch (Exception e) { }
+                    }
+                }
+                finally
+                {
+                    this.locker.Release();
+                }
+            } while (this.incoming);
+        }
+    }
+}
---
+++ Dao.QueueExecutor/QueueExecutor.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Dao.QueueExecutor
+{
+    public class QueueExecutor<TQueue, TResponse>
+    {
+        readonly ConcurrentQueue<TQueue> queue = new ConcurrentQueue<TQueue>();
+        readonly SemaphoreSlim locker;
+        volatile bool incoming;
+
+        public QueueExecutor(int concurrentCount = 1)
+        {
+            if (concurrentCount < 1)
+                concurrentCount = 1;
+
+            this.locker = new SemaphoreSlim(concurrentCount, concurrentCount);
+        }
+
+        /// <summary>
+        /// Set the executor to execute the queue.
+        /// </summary>
+        public event Func<TQueue, Task<TResponse>> Execute;
+        /// <summary>
+        /// Set the callback to handle the response of the executor
+        /// </summary>
+        public event Func<TQueue, TResponse, Task> Executed;
+        public event Action<Exception> OnException;
+
+        /// <summary>
+        /// Push anything to the queue, and wait for at least one executor to execute.
+        /// </summary>
+        /// <param name="data"></param>
+        public void Push(TQueue data)
+        {
+            this.queue.Enqueue(data);
+
+            this.incoming = true;
+
+            if (this.locker.CurrentCount < 1)
+                return;
+
+            Task.Run(Dequeue);
+        }
+
+        async Task<Func<TQueue, Task<TResponse>>> GetHandler()
+        {
+            while (true)
+            {
+                var result = Execute;
+                if (result != null)
+                    return result;
+
+                await Task.Delay(15).ConfigureAwait(false);
+            }
+        }
+
+        async Task Dequeue()
+        {
+            do
+            {
+                this.incoming = false;
+                await this.locker.WaitAsync().ConfigureAwait(false);
+                try
+                {
+                    while (this.queue.TryDequeue(out var data))
+                    {
+                        try
+                        {
+                            var execute = await GetHandler();
+                            var response = await execute(data);
+                            var executed = Executed;
+                            if (executed != null)
+                                await executed(data, response);
+                        }
+                        catch (Exception ex)
+                        {
+                            var onException = OnException;
+                            if (onException != null)
+                            {
+                                try
+                                {
+                                    onException(ex);
+                                }
+                                catch (Exception e) { }
+                            }
+                        }
+                    }
+                }
+                finally
+                {
+                    this.locker.Release();
+                }
+            } while (this.incoming);
+        }
+    }
+}
