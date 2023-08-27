7bc5f686fa3fb9dc193c38c6590372e778932955
Oscar Gu <oscar.gu@philips.com>
2023-08-27 19:51:21 +0800

    support .net7

---
+++ Dao.QueueExecutor/Extensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading;
+
+namespace Dao.QueueExecutor
+{
+    public static class Extensions
+    {
+        public static bool TestWait(this SemaphoreSlim source) => source.CurrentCount >= 1 && source.Wait(0);
+    }
+}
